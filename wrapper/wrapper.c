ffecdb132b53d6f2870c9d91c331f69f48ce04a0
GazPPN <gaspard.pepin@hotmail.fr>
2023-01-12 10:22:39 +0100

    correction wrapper2

--- wrapper/wrapper.c
+++ wrapper/wrapper.c
@@ -13,11 +13,16 @@ int main(int argc, char *argv[]){
     size_t shellcode_len = strlen(shellcode);
     printf("Shellcode size (bytes) : %ld\n", shellcode_len);
 
-    //execution shell code
-    int (*ret)() = (int(*)())shellcode;
+    printf("Shellcode : ");
+    for (size_t i = 0; i < shellcode_len; i++){
+        printf("\\x%02x", (unsigned char)shellcode[i]);
+    }
+    printf("\n");
 
-    ret();
-
+    printf("Schellcode execution");
+    int (*func)();
+    func = (int (*)()) shellcode;
+    (int)(*func)();
+    
     return 0;
-
 }
