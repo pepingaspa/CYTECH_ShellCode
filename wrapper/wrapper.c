7beada065f649de21a92dbed067af802d7b112c8
GazPPN <gaspard.pepin@hotmail.fr>
2023-01-12 10:11:32 +0100

    correction wrapper

--- wrapper/wrapper.c
+++ wrapper/wrapper.c
@@ -1,18 +1,23 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-unsigned char code[] =
- "\x6a\x3b\x58\x48\x31\xd2\x49"
- "\xb8\x2f\x2f\x62\x69\x6e\x2f"
- "\x73\x68\x49\xc1\xe8\x08\x41"
- "\x50\x48\x89\xe7\x52\x57\x48"
- "\x89\xe6\x0f\x05\x6a\x3c\x58"
- "\x48\x31\xff\x0f\x05";
+int main(int argc, char *argv[]){
+    if(argc < 2)
+        printf("error");
+        return 1;
 
-int main(int argc, char **argv) {
+    char *shellcode = argv[1];
 
-    int (*ret)() = (int(*)())code;
+    size_t shellcode_len = strlen(shellcode);
+    printf("Shellcode size (bytes) : %ld\n", shellcode_len);
+
+    //execution shell code
+    int (*ret)() = (int(*)())shellcode;
 
     ret();
+
+    return 0;
 
 }
